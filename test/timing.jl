04a563526ded4ac95f28ec049e7cb13434dd0cfb
Katharine Hyatt <kshyatt@physics.ucsb.edu>
2015-04-29 15:24:07 -0700

    Timing now makes pretty pictures

--- test/timing.jl
+++ test/timing.jl
@@ -1,18 +1,18 @@
 using CUSPARSE
 using CUDArt
 using Base.Test
+using Gadfly
 
 ms = [256,512,603,1024,1505,2048]
 ns = [512,1024,705,2048,1701,4096]
 ks = [128,256,256,512,512,2048]
 blockdims = [32,64,256,256,512,512,1024]
 types = [Float32, Float64, Complex64, Complex128]
-p = 0.2
+prob = 0.2
 
 # matrix * vector
-
-function mv(elty,m,n,bD)
-    A = convert(SparseMatrixCSC{elty,Int},sprand(m,n,p))
+function cpumv(elty,m,n,arr)
+    A = convert(SparseMatrixCSC{elty,Int},sprand(m,n,prob))
     x = rand(elty,n)
     y = rand(elty,m)
     alpha = rand(elty)
@@ -22,9 +22,15 @@ function mv(elty,m,n,bD)
         y = alpha * A * x + beta * y
     end
     ti = toq()
-    print("Time for ")
-    print_with_color(:blue,"CPU m*v ")
-    print("with dims ",m," ",n," and type ",elty,": ",ti,"\n")
+    return vcat(arr,[ti])
+end
+
+function csrmv(elty,m,n,arr)
+    A = convert(SparseMatrixCSC{elty,Int},sprand(m,n,prob))
+    x = rand(elty,n)
+    y = rand(elty,m)
+    alpha = rand(elty)
+    beta  = rand(elty)
     tic()
     d_A = CudaSparseMatrixCSR(A)
     d_x = CudaArray(x)
@@ -34,20 +40,15 @@ function mv(elty,m,n,bD)
     end
     h_y = to_host(d_y)
     ti = toq()
-    print("Time for ")
-    print_with_color(:purple,"CSR m*v ")
-    print("with dims ",m," ",n," and type ",elty,": ",ti,"\n")
-    tic()
-    d_A = CudaSparseMatrixCSR(A)
-    d_A = CUSPARSE.switch2bsr(d_A,convert(Cint,bD))
-    d_x = CudaArray(x)
-    d_y = CudaArray(y)
-    for i in 1:20
-        d_y = CUSPARSE.bsrmv!('N',alpha,d_A,d_x,beta,d_y,'O')
-    end
-    h_y = to_host(d_y)
-    ti = toq()
-    println("Time for BSR m*v with dims ",m," ",n," and type ",elty,": ",ti)
+    return vcat(arr,[ti])
+end
+
+function hybmv(elty,m,n,arr)
+    A = convert(SparseMatrixCSC{elty,Int},sprand(m,n,prob))
+    x = rand(elty,n)
+    y = rand(elty,m)
+    alpha = rand(elty)
+    beta  = rand(elty)
     tic()
     d_A = CudaSparseMatrixCSR(A)
     d_A = CUSPARSE.switch2hyb(d_A)
@@ -58,21 +59,36 @@ function mv(elty,m,n,bD)
     end
     h_y = to_host(d_y)
     ti = toq()
-    println("Time for HYB m*v with dims ",m," ",n," and type ",elty,": ",ti)
+    return vcat(arr,[ti])
 end
 
-for (m,n,bD) in zip(ms,ns,blockdims)
-    for ty in types
-        mv(ty,m,n,bD)
+p = []
+for ty in types
+    mv = Gadfly.DataFrame()
+    mvcputimes = Any[]
+    mvcsrtimes = Any[]
+    mvhybtimes = Any[]
+    xind = Any[]
+    for (m,n) in zip(ms,ns)
+        mvcputimes = cpumv(ty,m,n,mvcputimes)
+        mvcsrtimes = csrmv(ty,m,n,mvcsrtimes)
+        mvhybtimes = hybmv(ty,m,n,mvhybtimes)
+        xind = vcat(xind,(m,n))
     end
+    mv = vcat(mv, Gadfly.DataFrame(x = xind, y = ones(length(ms)), Format=string("CPU")))
+    mv = vcat(mv, Gadfly.DataFrame(x = xind, y = mvcputimes./mvcsrtimes, Format=string("CSR")))
+    mv = vcat(mv, Gadfly.DataFrame(x = xind, y = mvcputimes./mvhybtimes, Format=string("HYB")))
+    p = vcat(p,Gadfly.plot(mv, x="x",y="y",color="Format",Stat.yticks([0:5:40]),Scale.x_discrete,Geom.bar(position=:dodge),Guide.xlabel("Matrix size"),Guide.ylabel("Speedup"),Guide.title(string("Speed comparison for 20 sm * dv multiplications with precision ",ty))))
 end
+
+draw(PDF(("mv.pdf"),15inch,15inch),vstack(hstack(p[1],p[2]),hstack(p[3],p[4])))
 
 # matrix/vector solve
 
-function sv(elty,m,bD)
-    A = triu(sprand(m,m,p))
+function cpusv(elty,m,arr)
+    A = triu(sprand(m,m,prob))
     A += speye(m)
-    A = convert(SparseMatrixCSC{elty,Int},A)
+    A = convert(SparseMatrixCSC{elty,Cint},A)
     X = rand(elty,m)
     alpha = rand(elty)
     tic()
@@ -81,9 +97,15 @@ function sv(elty,m,bD)
         y = full(A)\(alpha*X)
     end
     ti = toq()
-    print("Time for ")
-    print_with_color(:blue,"CPU sv ")
-    print("with dim ",m," and type ",elty,": ",ti,"\n")
+    return vcat(arr,[ti])
+end
+
+function csrsv(elty,m,arr)
+    A = triu(sprand(m,m,prob))
+    A += speye(m)
+    A = convert(SparseMatrixCSC{elty,Cint},A)
+    X = rand(elty,m)
+    alpha = rand(elty)
     tic()
     d_A = CudaSparseMatrixCSR(A)
     d_X = CudaArray(X)
@@ -92,19 +114,15 @@ function sv(elty,m,bD)
     end
     h_X = to_host(d_X)
     ti = toq()
-    print("Time for ")
-    print_with_color(:purple,"CSR sv ")
-    print("with dim ",m," and type ",elty,": ",ti,"\n")
-    tic()
-    d_A = CudaSparseMatrixCSR(A)
-    d_A = CUSPARSE.switch2bsr(d_A,convert(Cint,bD))
-    d_X = CudaArray(X)
-    for i in 1:20
-        d_X = CUSPARSE.bsrsv2!('N',alpha,d_A,d_X,'O')
-    end
-    h_X = to_host(d_X)
-    ti = toq()
-    println("Time for BSR sv with dim ",m," and type ",elty,": ",ti)
+    return vcat(arr,[ti])
+end
+
+function hybsv(elty,m,arr)
+    A = triu(sprand(m,m,prob))
+    A += speye(m)
+    A = convert(SparseMatrixCSC{elty,Cint},A)
+    X = rand(elty,m)
+    alpha = rand(elty)
     tic()
     d_A = CudaSparseMatrixCSR(A)
     d_A = CUSPARSE.switch2hyb(d_A)
@@ -115,19 +133,34 @@ function sv(elty,m,bD)
     end
     h_X = to_host(d_X)
     ti = toq()
-    println("Time for HYB sv with dim ",m," and type ",elty,": ",ti)
+    return vcat(arr,[ti])
 end
 
-for (m,bD) in zip(ms,blockdims)
-    for ty in types
-        sv(ty,m,bD)
+p = []
+for ty in types
+    sv = Gadfly.DataFrame()
+    svcputimes = Any[]
+    svcsrtimes = Any[]
+    svhybtimes = Any[]
+    xind = Any[]
+    for (m,n) in zip(ms,ns)
+        svcputimes = cpusv(ty,m,svcputimes)
+        svcsrtimes = csrsv(ty,m,svcsrtimes)
+        svhybtimes = hybsv(ty,m,svhybtimes)
+        xind = vcat(xind,(m,n))
     end
+    sv = vcat(sv, Gadfly.DataFrame(x = xind, y = ones(length(ms)), Format=string("CPU")))
+    sv = vcat(sv, Gadfly.DataFrame(x = xind, y = svcputimes./svcsrtimes, Format=string("CSR")))
+    sv = vcat(sv, Gadfly.DataFrame(x = xind, y = svcputimes./svhybtimes, Format=string("HYB")))
+    p = vcat(p,Gadfly.plot(sv, x="x",y="y",color="Format",Stat.yticks([0:5:40]),Scale.x_discrete,Geom.bar(position=:dodge),Guide.xlabel("Matrix size"),Guide.ylabel("Speedup"),Guide.title(string("Speed comparison for 20 sm \ dv solutions with precision ",ty))))
 end
+
+draw(PDF(("sv.pdf"),15inch,15inch),vstack(hstack(p[1],p[2]),hstack(p[3],p[4])))
 
 # matrix * matrix
 
-function mm(elty,m,k,n,bD)
-    A = convert(SparseMatrixCSC{elty,Int},sprand(m,k,p))
+function cpumm(elty,m,k,n,arr)
+    A = convert(SparseMatrixCSC{elty,Int},sprand(m,k,prob))
     B = rand(elty,k,n)
     C = rand(elty,m,n)
     alpha = rand(elty)
@@ -137,9 +170,15 @@ function mm(elty,m,k,n,bD)
         C = alpha * A * B + beta * C
     end
     ti = toq()
-    print("Time for ")
-    print_with_color(:blue,"CPU m*m ")
-    print("with dims ",m," ",n," and type ",elty,": ",ti,"\n")
+    return vcat(arr,[ti])
+end
+
+function csrmm(elty,m,k,n,arr)
+    A = convert(SparseMatrixCSC{elty,Int},sprand(m,k,prob))
+    B = rand(elty,k,n)
+    C = rand(elty,m,n)
+    alpha = rand(elty)
+    beta  = rand(elty)
     tic()
     d_A = CudaSparseMatrixCSR(A)
     d_B = CudaArray(B)
@@ -149,26 +188,23 @@ function mm(elty,m,k,n,bD)
     end
     h_C = to_host(d_C)
     ti = toq()
-    print("Time for ")
-    print_with_color(:purple,"CSR m*m ")
-    print("with dims ",m," ",n," and type ",elty,": ",ti,"\n")
-    tic()
-    d_A = CudaSparseMatrixCSR(A)
-    d_A = CUSPARSE.switch2bsr(d_A,convert(Cint,bD))
-    d_B = CudaArray(B)
-    d_C = CudaArray(C)
-    for i in 1:20
-        d_C = CUSPARSE.bsrmm!('N','N',alpha,d_A,d_B,beta,d_C,'O')
-    end
-    h_C = to_host(d_C)
-    ti = toq()
-    println("Time for BSR m*m with dims ",m," ",n," and type ",elty,": ",ti)
-    tic()
+    return vcat(arr,[ti])
 end
 
-for (m,n,k,bD) in zip(ms,ns,ks,blockdims)
-    for ty in types
-        mm(ty,m,k,n,bD)
+p = []
+for ty in types
+    mm = Gadfly.DataFrame()
+    mmcputimes = Any[]
+    mmcsrtimes = Any[]
+    xind = Any[]
+    for (m,k,n) in zip(ms,ks,ns)
+        mmcputimes = cpumm(ty,m,k,n,mmcputimes)
+        mmcsrtimes = csrmm(ty,m,k,n,mmcsrtimes)
+        xind = vcat(xind,(m,k,n))
     end
+    mm = vcat(mm, Gadfly.DataFrame(x = xind, y = ones(length(ms)), Format=string("CPU")))
+    mm = vcat(mm, Gadfly.DataFrame(x = xind, y = mmcputimes./mmcsrtimes, Format=string("CSR")))
+    p = vcat(p,Gadfly.plot(mm, x="x",y="y",color="Format",Stat.yticks([0:5:50]),Scale.x_discrete,Geom.bar(position=:dodge),Guide.xlabel("Matrix sizes"),Guide.ylabel("Speedup"),Guide.title(string("Speed comparison for 20 sm * sm multiplications with precision ",ty))))
 end
 
+draw(PDF(("mm.pdf"),15inch,15inch),vstack(hstack(p[1],p[2]),hstack(p[3],p[4])))
